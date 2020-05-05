3bee40aee12b84e7a5149331102596083758782a
mostafaaminflakes <ms.amin@gmail.com>
2020-05-06 00:53:00 +0300

    Initial commit

---
+++ firestore/FirestoreConfig.js
@@ -0,0 +1,12 @@
+const FirebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+};
+
+export default FirebaseConfig;
